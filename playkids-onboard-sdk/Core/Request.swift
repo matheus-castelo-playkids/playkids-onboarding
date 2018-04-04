d2a29e2861c957369df470baf0ee614a52c5cb1e
matheus-castelo-playkids <matheus.castelo@playkids.com>
2018-04-04 14:39:24 -0300

    Creation of the sdk

---
+++ playkids-onboard-sdk/Core/Request.swift
@@ -0,0 +1,10 @@
+//
+//  Request.swift
+//  playkids-onboard-sdk
+//
+//  Created by Matheus Castelo on 04/04/18.
+//  Copyright © 2018 Matheus Castelo. All rights reserved.
+//
+
+import Foundation
+import Alamofire
---
+++ playkids-onboard-sdk/playkids_onboard_sdk.h
@@ -0,0 +1,17 @@
+//
+//  playkids_onboard_sdk.h
+//  playkids-onboard-sdk
+//
+//  Created by Matheus Castelo on 04/04/18.
+//  Copyright © 2018 Matheus Castelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for playkids_onboard_sdk.
+FOUNDATION_EXPORT double playkids_onboard_sdkVersionNumber;
+
+//! Project version string for playkids_onboard_sdk.
+FOUNDATION_EXPORT const unsigned char playkids_onboard_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <playkids_onboard_sdk/PublicHeader.h>
